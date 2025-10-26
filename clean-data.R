c73802ab29c085022c412f2760ac748f96e3d756
EconMaett <matthias.spichiger@bluewin.ch>
2025-10-26 19:03:29 +0100

    changed span and alpha in geom_smooth to 0.1

--- clean-data.R
+++ clean-data.R
@@ -1,3 +1,4 @@
+# clean-data.R ----
 ## load packages ----
 library(tidyverse)
 
@@ -13,9 +14,9 @@ write_csv(x = nilometer, file = "nilometer.csv")
 nilometer_caption <- "D. Koutsoyiannis, Hydrology and Change, Hydrological Sciences Journal, 58 (6), 1177–1197, doi:10.1080/02626667.2013.804626, 2013."
 
 p <- ggplot(data = nilometer, mapping = aes(x = year, y = min_level)) +
-  geom_line(linewidth = 0.5, colour = "gray", alpha = 0.5) +
+  geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#00b4d8", fill = "#00b4d8", alpha = 0.1, span = 0.1) +
+  geom_line(linewidth = 0.5, colour = "gray", alpha = 0.25) +
   geom_point(size = 0.75) +
-  # geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#00b4d8", fill = "#00b4d8") +
   scale_x_continuous(breaks = seq(600, 1950, 50), limits = c(600, 1950)) +
   scale_y_continuous(breaks = seq(8, 16, 2), limits = c(8, 16)) +
   labs(
