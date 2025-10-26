3e3aafc964d7e23affdb058c28007158517439dd
EconMaett <matthias.spichiger@bluewin.ch>
2025-10-26 12:50:30 +0100

    increase image size

--- clean-data.R
+++ clean-data.R
@@ -14,8 +14,8 @@ write_csv(x = nilometer, file = "nilometer.csv")
 nilometer_caption <- "D. Koutsoyiannis, Hydrology and Change, Hydrological Sciences Journal, 58 (6), 1177–1197, doi:10.1080/02626667.2013.804626, 2013."
 
 p <- ggplot(data = nilometer, mapping = aes(x = year)) +
-  geom_line(mapping = aes(y = min_level), linewidth = 0.25) +
-  geom_point(mapping = aes(y = min_level), size = 0.5) +
+  geom_line(mapping = aes(y = min_level), linewidth = 0.5) +
+  geom_point(mapping = aes(y = min_level), size = 1) +
   scale_x_continuous(breaks = seq(600, 1950, 150), limits = c(600, 1950)) +
   scale_y_continuous(breaks = seq(8, 16, 2), limits = c(8, 16)) +
   labs(
@@ -28,5 +28,5 @@ p <- ggplot(data = nilometer, mapping = aes(x = year)) +
 
 print(p)
 
-ggsave(filename = "nilometer.png", path = ".", plot = p, width = 14, height = 8, units = "cm", bg = "white")
+ggsave(filename = "nilometer.png", path = ".", plot = p, width = 24, height = 10, units = "cm", bg = "white")
 
